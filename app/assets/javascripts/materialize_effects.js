2901a9c03cf956bf61e2d3074ef838ab4d01230e
José Carlos Anaya Chávez <josecarlosanach@MacBook-Pro-de-Jose.local>
2018-11-29 10:54:42 -0600

    suggested changes added and internationalization

--- app/assets/javascripts/materialize_effects.js
+++ app/assets/javascripts/materialize_effects.js
@@ -2,7 +2,6 @@ function setMaterialize(element, component, op) {
     var elems = document.querySelectorAll(element);
     var instances = M[component].init(elems, op);
  }
- 
  document.addEventListener('DOMContentLoaded', function() {
     setMaterialize('.slider', 'Slider', {});
     setMaterialize('.sidenav', 'Sidenav', {});
