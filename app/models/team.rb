9ac9f2e6a2dee44df3eaef7ecdf768247944999f
fernando amezcua <fernandoamezcua@Mac-mini-de-fernando.local>
2018-12-28 12:47:07 -0600

    fix changes

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,3 @@
 class Team < ApplicationRecord
-  validates :name, :principal_position, :start_date, :end_date, presence: true
+   validates :name, :principal_position, :start_date, :end_date, presence: true	
 end
