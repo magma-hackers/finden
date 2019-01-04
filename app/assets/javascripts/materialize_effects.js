c0487d6d56d70c7a2fe0b9c97bcaca16836950ff
José Carlos Anaya Chávez <josecarlosanach@MacBook-Pro-de-Jose.local>
2019-01-04 13:13:05 -0600

    ticket #63 added a dropdown to organize the menu

--- app/assets/javascripts/materialize_effects.js
+++ app/assets/javascripts/materialize_effects.js
@@ -1,8 +1,14 @@
 function setMaterialize(element, component, op) {
-    var elems = document.querySelectorAll(element);
-    var instances = M[component].init(elems, op);
- }
- document.addEventListener('DOMContentLoaded', function() {
-    setMaterialize('.slider', 'Slider', {});
-    setMaterialize('.sidenav', 'Sidenav', {});
- }); 
+   var elems = document.querySelectorAll(element);
+   var instances = M[component].init(elems, op);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+   setMaterialize('.slider', 'Slider', {});
+   setMaterialize('.sidenav', 'Sidenav', {});
+}); 
+ 
+document.addEventListener("turbolinks:load", function() {
+ M.AutoInit();
+ $(".dropdown-trigger").dropdown();
+});
