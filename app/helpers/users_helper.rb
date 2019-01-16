2f040d4116a0179406783d4ce00ba91b7b7e0039
fernando amezcua <fernandoamezcua@Mac-mini-de-fernando.local>
2019-01-15 18:22:34 -0600

    add favicon and description of images to upload

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,12 @@
 module UsersHelper
-    def define_image
-        if resource.avatar.attached? 
-            image_tag(resource.avatar.variant(resize: "150x150"))
-        elsif resource.image
-            image_tag(resource.image) 
-        else
-            value_without_image = t('helper.without_image').html_safe
-        end
-    end
+	def define_image
+		if resource.avatar.attached? 
+			image_tag(resource.avatar.variant(resize: "150x150"))
+		end
+		if resource.image
+			image_tag(resource.image) 
+		else
+			value_without_image = t('helper.without_image').html_safe
+		end
+	end
 end
